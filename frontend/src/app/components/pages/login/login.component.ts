bd887c70cbfe167a053d1c9986eab2064395fcc0
IvNorbi <ivadi.norbi@gmail.com>
2023-12-01 15:35:21 +0100

    login bug.

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm!:FormGroup;
+  isSubmitted = false;
+
+  // constructor(private formBuilder : FormBuilder) { } kivettem
 
 }
