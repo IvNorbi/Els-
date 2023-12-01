ca04bba6c6f86436a059644ebc2cde2bef9072cb
IvNorbi <ivadi.norbi@gmail.com>
2023-12-01 19:14:50 +0100

    test

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -11,6 +11,6 @@ export class LoginComponent {
   loginForm!:FormGroup;
   isSubmitted = false;
 
-  // constructor(private formBuilder : FormBuilder) { } kivettem
+  // constructor(private formBuilder : FormBuilder) { }
 
 }
