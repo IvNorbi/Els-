8b00f167b789028df8b817aeeb95988595cbbf81
IvNorbi <ivadi.norbi@gmail.com>
2024-02-13 01:13:36 +0100

    Keresési komponens frontendre terhelése.
    	modified:   frontend/src/app/components/pages/movie-page/movie-page.component.html
    	modified:   frontend/src/app/components/partials/search/search.component.ts
    	modified:   frontend/src/app/services/filmek.service.ts

--- frontend/src/app/components/partials/search/search.component.ts
+++ frontend/src/app/components/partials/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FilmekService } from 'src/app/services/filmek.service';
 
 @Component({
   selector: 'app-search',
@@ -9,10 +10,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class SearchComponent implements OnInit {
   searchTerm = '';
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private filmService: FilmekService) {
     this.activatedRoute.params.subscribe((params) => {
       if (params.searchTerm) {
         this.searchTerm = params.searchTerm;
+        this.search(params.searchTerm); // Automatikusan végrehajtjuk a keresést, ha van paraméter
       }
     });
   }
@@ -23,6 +25,9 @@ export class SearchComponent implements OnInit {
   search(term: string): void {
     if (term) {
       this.router.navigateByUrl('/search/' + term);
+      this.filmService.getAllFilmBySearchTerm(term).subscribe((results: any[]) => {
+        // Itt kezeld a keresés eredményeit
+      });
     } else {
       this.router.navigateByUrl('/home');
     }
