64552b8afd0dcf953ac17bf8e5d80d97a48721b9
IvNorbi <ivadi.norbi@gmail.com>
2024-03-10 02:14:41 +0100

    Carousel feltöltése fő oldalon. -Alapértelmezett főoldal megváltoztatva.

--- frontend/src/app/components/pages/main/main.component.ts
+++ frontend/src/app/components/pages/main/main.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmekService } from 'src/app/services/filmek.service';
+import { Film } from 'src/app/shared/models/filmek';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  movies: Film[] = [];
 
+  constructor(private filmekService: FilmekService) { }
+
+  ngOnInit(): void {
+    this.fetchRandomMovies();
+  }
+
+  fetchRandomMovies(): void {
+    this.filmekService.getRandomMovies()
+      .subscribe(movies => {
+        this.movies = movies;
+      });
+  }
 }
